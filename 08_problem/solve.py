00f257ba7b25f82c95a29b73ff35d7afb60b07a7
ath0mos <athanasios.tsarapatsanis@outlook.de>
2026-01-06 14:05:08 +0100

    Solving day 8 Implementing Union-Find data structure to represent circuits

---
+++ 08_problem/solve.py
@@ -0,0 +1,25 @@
+# Implementing Union-Find data structure to represent the groups of circuits,
+# because the main operations needed are merge and find.
+class DSU:
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.size = [1] * n
+
+    def find(self, a: int) -> int:
+        while self.parent[a] != a:
+            self.parent[a] = self.parent[self.parent[a]]
+            a = self.parent[a]
+        return a
+
+    def union(self, a: int, b: int) -> bool:
+        ra = self.find(a)
+        rb = self.find(b)
+        if ra == rb:
+            return False
+        # Connect the smaller tree/circuit to the bigger one
+        # to make search faster
+        if self.size[ra] < self.size[rb]:
+            ra, rb = rb, ra
+        self.parent[rb] = ra
+        self.size[ra] += self.size[rb]
+        return True
